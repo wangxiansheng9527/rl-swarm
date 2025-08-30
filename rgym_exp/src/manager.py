44ffa5a3c0f1c159109de246d37d9670b23d7eaa
wangxiansheng9527 <79587255+wangxiansheng9527@users.noreply.github.com>
2025-08-30 17:03:13 +0800

    Update manager.py

--- rgym_exp/src/manager.py
+++ rgym_exp/src/manager.py
@@ -87,7 +87,7 @@ class SwarmGameManager(BaseGameManager, DefaultGameManagerMixin):
 
         self.batched_signals = 0.0
         self.time_since_submit = time.time()  # seconds
-        self.submit_period = 3.0  # hours
+        self.submit_period = 1.0  # hours
         self.submitted_this_round = False
 
         # PRG Game
