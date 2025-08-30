1c04fb35b19f9572381732e285dc30ac141e3fe6
wangxiansheng9527 <79587255+wangxiansheng9527@users.noreply.github.com>
2025-08-30 17:04:24 +0800

    Update run_rl_swarm.sh
    注释掉登陆

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -160,15 +160,15 @@ if [ "$CONNECT_TO_TESTNET" = true ]; then
     sleep 5
 
     # Try to open the URL in the default browser
-    if [ -z "$DOCKER" ]; then
-        if open http://localhost:3000 2> /dev/null; then
-            echo_green ">> Successfully opened http://localhost:3000 in your default browser."
-        else
-            echo ">> Failed to open http://localhost:3000. Please open it manually."
-        fi
-    else
-        echo_green ">> Please open http://localhost:3000 in your host browser."
-    fi
+    # if [ -z "$DOCKER" ]; then
+    #    if open http://localhost:3000 2> /dev/null; then
+    #        echo_green ">> Successfully opened http://localhost:3000 in your default browser."
+    #    else
+    #        echo ">> Failed to open http://localhost:3000. Please open it manually."
+    #    fi
+    # else
+    #    echo_green ">> Please open http://localhost:3000 in your host browser."
+    # fi
 
     cd ..
 
